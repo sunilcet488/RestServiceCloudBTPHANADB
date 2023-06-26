50e4f46145849844828c3be1bc4b6097c9ca41b9
sunilcet488 <sunilcet488@gmail.com>
2023-06-26 20:52:25 +0530

    Delete RestServiceBTPHDI_XSUAA directory

--- RestServiceBTPHDI_XSUAA/src/main/java/com/abapskill/services/IAddressPersistance.java
+++
@@ -1,9 +0,0 @@
-package com.abapskill.services;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.abapskill.entities.Address;
-
-public interface IAddressPersistance extends JpaRepository<Address, String>{
-	
-}
--- RestServiceBTPHDI_XSUAA/src/main/resources/static/controller/App.controller.js
+++
@@ -1,9 +0,0 @@
-sap.ui.define(
-	["sap/ui/core/mvc/Controller"],
-	function(controller){
-	
-	return controller.extend("myFirstApp.controller.App",{
-		
-	});
-		
-});
